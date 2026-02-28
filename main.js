b6ff4c6ae55e1be6045531de292f405a62b4ba42
ebonyandivory84 <136506320+ebonyandivory84@users.noreply.github.com>
2026-02-28 16:33:53 +0100

    Fix dual entrypoint for ioBroker and Expo

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-module.exports = require("./adapter/main");
+if (typeof navigator !== "undefined" && navigator.product === "ReactNative") {
+  require("./index");
+} else {
+  module.exports = eval("require")("./adapter/main");
+}
