7712720396018574c8fecf70dc9211efcbf5e4bb
ebonyandivory84 <136506320+ebonyandivory84@users.noreply.github.com>
2026-03-01 01:39:12 +0100

    Use native widget rendering for compact mobile web

--- src/components/GridCanvas.tsx
+++ src/components/GridCanvas.tsx
@@ -66,7 +66,7 @@ export function GridCanvas({
   };
 
   const content =
-    Platform.OS === "web" ? (
+    Platform.OS === "web" && !isCompactWeb ? (
       <WebGridCanvas
         canvasHeight={canvasHeight}
         cellWidth={cellWidth}
